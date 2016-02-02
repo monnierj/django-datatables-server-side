13f9ba9d1026ca068555fa939b2e1bf13697f8aa
Julien Monnier <jumonnier@gmail.com>
2016-02-02 16:47:26 +0100

    Setup script added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='django_datatables_server_side',
+      version='0.1',
+      description="Simple Datatables' Server-side processing view for Django",
+      author="Julien Monnier",
+      author_email='jumonnier@gmail.com',
+      packages=['django_datatables_server_side'],
+      install_requires=['Django>=1.8']
+      )
